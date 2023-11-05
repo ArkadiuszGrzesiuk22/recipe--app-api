d87bf6b3e578c6e9f761d1981587b3eff7fdd148
ArkadiuszGrzesiuk22 <arkadiuszgrzesiuk22@gmail.com>
2023-11-05 15:50:40 +0100

    Fixing flake8 warnings about syntax

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,7 +1,4 @@
-"""
-Test custom Django commands 
-"""
-
+"""Test custom Django commands"""
 from unittest.mock import patch
 
 from psycopg2 import OperationalError as Psycopg2OpError
@@ -11,10 +8,8 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready!"""
         patched_check.return_value = True
@@ -26,9 +21,10 @@ class CommandTests(SimpleTestCase):
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting Operational Error"""
-        patched_check.side_effect = [Psycopg2OpError] * 2 + [OperationalError] * 3 + [True]
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
+                                    [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
-        
+
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
